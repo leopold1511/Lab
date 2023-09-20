84a7e6eaa8a1ffcb090a66940b179a98e1bef97a
leopold1511 <98979188+leopold1511@users.noreply.github.com>
2023-09-20 22:34:17 +0300

    Update Main.java

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -2,8 +2,7 @@ import Subpackage.Food;
 import Subpackage.Man;
 
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
         Food[] dinner=new Food[4];
