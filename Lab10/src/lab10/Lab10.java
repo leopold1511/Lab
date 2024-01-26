fabc8c1b7d296472610de2d35665b9061e6f28e3
leopold1511 <98979188+leopold1511@users.noreply.github.com>
2024-01-26 10:50:54 +0300

    Add files via upload

---
+++ Lab10/src/lab10/Lab10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab10;
+
+/**
+ *
+ * @author SystemX
+ */
+public class Lab10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AutoService GUI=new AutoService();
+        GUI.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
